63ffedfb5b1852f4786a66ac57d8aafc15a6f98d
JongHyuckLee <yarn add --dev --exact prettier>
2024-04-05 01:55:37 +0900

    Starfield 만들기 5

---
+++ section1-transition/05/Star.js
@@ -0,0 +1,12 @@
+export default class Star {
+  constructor(index, x, y, z) {
+    const element = document.createElement("div");
+    element.classList.add("star");
+    element.innerHTML = index;
+    console.log({ x, y, z });
+    element.style.left = `${x}px`;
+    element.style.top = `${y}px`;
+    element.style.transform = `scale(${z})`;
+    document.body.appendChild(element);
+  }
+}
