357fcc28e7eef09ac5d12622ade549689c52f9a9
JongHyuckLee <yarn add --dev --exact prettier>
2024-04-14 20:20:22 +0900

    스크롤 타임라인 이해하기 2

--- section1-transition/07/01-basic/main.js
+++ section1-transition/07/01-basic/main.js
@@ -1,9 +1,9 @@
 import "./scroll-timeline.js";
 
 const progress = document.querySelector(".progress");
-const scrollBox = document.querySelector(".scroll-box");
+// const scrollBox = document.querySelector(".scroll-box");
+const targetImages = document.querySelectorAll(".target-image");
 
-console.log(document.body);
 progress.animate(
   [
     {
@@ -16,11 +16,28 @@ progress.animate(
   {
     timeline: new ScrollTimeline({
       scrollOffsets: [
-        // { target: document.body, edge: "start", threshold: 1 },
-        // { target: document.body, edge: "end", threshold: 1 },
-        { target: scrollBox, edge: "start", threshold: 1 },
-        { target: scrollBox, edge: "end", threshold: 1 },
+        { target: document.body, edge: "start", threshold: 1 },
+        { target: document.body, edge: "end", threshold: 1 },
+        // { target: scrollBox, edge: "start", threshold: 1 },
+        // { target: scrollBox, edge: "end", threshold: 1 },
       ],
     }),
   }
 );
+console.log(targetImages);
+targetImages.forEach((image) => {
+  const imageTop = image.offsetTop;
+  const imageHeight = image.offsetHeight;
+  const offset1 = imageTop + imageHeight - window.innerHeight;
+  console.log(imageTop + imageHeight, offset1);
+  const offset2 = imageTop - 200;
+  console.log(offset1, offset2);
+  image.animate([{ opacity: 0 }, { opacity: 1 }], {
+    timeline: new ScrollTimeline({
+      scrollOffsets: [
+        new CSSUnitValue(offset1, "px"),
+        new CSSUnitValue(offset2, "px"),
+      ],
+    }),
+  });
+});
